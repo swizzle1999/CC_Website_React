6537602960d55f65a50478130a8bbcd49bd0de7e
swizzle1999 <key.key@hotmail.co.uk>
2026-05-01 21:30:03 +0100

    Implemented the post component onto the home page

--- cc_website_react.client/src/App.jsx
+++ cc_website_react.client/src/App.jsx
@@ -1,9 +1,19 @@
 import './App.scss';
+import { useEffect, useState } from 'react';
+import Post from './components/Post.jsx';
 
 function App() {
+    const [posts, setPosts] = useState([]);
+    useEffect(() => {
+        fetch('../posts/index.json').then(r => r.json()).then(data => setPosts(data.slice(0, 1)));
+        console.log(posts)
+    }, []);
+
     return (
         <div>
-            <h1>Gaming 😎</h1>
+            {posts.map(post => (
+                <Post key={post.slug} post={post} />
+            ))}
         </div>
     );
 }
