7c764b7b0efee9b0758571a6f2bd23c009426019
swizzle1999 <key.key@hotmail.co.uk>
2026-05-01 21:12:20 +0100

    Moved post html into its own reusable component

---
+++ cc_website_react.client/src/components/Post.jsx
@@ -0,0 +1,20 @@
+import './Post.scss';
+import { Link } from 'react-router';
+import { Col } from 'react-bootstrap';
+
+export default function Post({ post }) {
+    return (
+        <Col key={post.slug} xs={12} sm={6} lg={4}>
+            <Link className="post-item d-flex flex-column h-100" to={'/news/' + post.slug}>
+                <div className="post-thumbnail-wrapper">
+                    <img src={`../posts/${post.slug}/${post.thumbnail}`} alt={post.title} className="post-thumbnail" />
+                </div>
+                <div className="p-2 d-flex flex-column flex-grow-1">
+                    <div className="post-title">{post.title}</div>
+                    <div className="post-description mt-1">{post.description}</div>
+                    <div className="post-date mt-auto pt-2">🗓️ {post.date}</div>
+                </div>
+            </Link>
+        </Col>
+    );
+}
--- cc_website_react.client/src/components/PostList.jsx
+++ cc_website_react.client/src/components/PostList.jsx
@@ -1,11 +1,10 @@
 import './PostList.scss';
 import { useEffect, useState } from 'react';
-import { Link } from 'react-router';
-import { Row, Col } from 'react-bootstrap';
+import { Row } from 'react-bootstrap';
+import Post from './Post.jsx';
 
 export default function PostList() {
     const [posts, setPosts] = useState([]);
-    // TODO: 
     useEffect(() => {
         fetch('../posts/index.json').then(r => r.json()).then(setPosts);
     }, []);
@@ -14,18 +13,7 @@ export default function PostList() {
         <div className="post-items mt-2">
             <Row className="g-3">
                 {posts.map(p =>
-                    <Col key={p.slug} xs={12} sm={6} lg={4}>
-                        <Link className="post-item d-flex flex-column h-100" to={'/news/' + p.slug}>
-                            <div className="post-thumbnail-wrapper">
-                                <img src={`../posts/${p.slug}/${p.thumbnail}`} alt={p.title} className="post-thumbnail" />
-                            </div>
-                            <div className="p-2 d-flex flex-column flex-grow-1">
-                                <div className="post-title">{p.title}</div>
-                                <div className="post-description mt-1">{p.description}</div>
-                                <div className="post-date mt-auto pt-2">🗓️ {p.date}</div>
-                            </div>
-                        </Link>
-                    </Col>
+                    <Post key={p.slug} post={ p }/>
                 )}
             </Row>
         </div>
