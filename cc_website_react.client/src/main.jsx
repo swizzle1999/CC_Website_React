d86a3b5adab7efef7ba8892acde49309181dfec7
swizzle1999 <key.key@hotmail.co.uk>
2026-05-04 16:22:11 +0100

    Updated index URL to use the environments base url

--- cc_website_react.client/src/main.jsx
+++ cc_website_react.client/src/main.jsx
@@ -16,7 +16,7 @@ createRoot(document.getElementById('root')).render(
             <Navbar />
             <div className="mx-3">
                 <Routes>
-                    <Route index element={<App />} />
+                    <Route path={import.meta.env.BASE_URL} element={<App />} />
                     {/*<Route path="about" element={<About />} />*/}
                     <Route path="news" element={<PostList />} />
                     <Route path="news/:slug" element={<PostPage />} />
