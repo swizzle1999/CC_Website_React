44d7d7bd537aa93440c7742c922bfc52a89dc686
swizzle1999 <key.key@hotmail.co.uk>
2026-05-05 11:56:50 +0100

    Fixed some styling issues, mainly on the PostPage. Also updated the sample data to be actually usefull

--- cc_website_react.client/src/main.jsx
+++ cc_website_react.client/src/main.jsx
@@ -3,10 +3,8 @@ import { createRoot } from 'react-dom/client'
 import './index.scss'
 import App from './App.jsx'
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import { Container } from 'react-bootstrap';
 import { BrowserRouter, Routes, Route } from 'react-router';
 import Navbar from './components/MainNavbar.jsx';
-import About from './components/About.jsx'
 import PostList from './components/PostList.jsx'
 import PostPage from './components/PostPage.jsx'
 
@@ -17,7 +15,6 @@ createRoot(document.getElementById('root')).render(
             <div className="mx-3">
                 <Routes>
                     <Route path="/" element={<App />} />
-                    {/*<Route path="about" element={<About />} />*/}
                     <Route path="news" element={<PostList />} />
                     <Route path="news/:slug" element={<PostPage />} />
                 </Routes>
